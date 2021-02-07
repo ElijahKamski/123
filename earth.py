bef89e4d2a7d3d09a32311fd45b9cb9a9cb35e4b
TAPAKAHOKOT <igrok332233@mail.ru>
2021-02-08 00:00:57 +0300

    1338.1| FIX: more earth and stars and bigger space in earh.py

--- earth.py
+++ earth.py
@@ -1,7 +1,7 @@
 from random import choice as ch 
 
-arr = ["Start"] * 500000
-arr.append("Earth")
+arr = ["Start"] * 50000
+[arr.append("Earth") for k in range(500)]
 
 
-print(" ".join([ch(arr) for k in range(1245212)]))
+print(" ".join([ch(arr) for k in range(1245212223)]))
